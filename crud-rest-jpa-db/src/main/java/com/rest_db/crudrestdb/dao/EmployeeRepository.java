b1bb6e7b0c1403794fce1ce0c46b139579cca1fc
akhushal <khushal0042@gmail.com>
2024-04-09 22:27:37 +0530

    deleted

--- crud-rest-jpa-db/src/main/java/com/rest_db/crudrestdb/dao/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rest_db.crudrestdb.dao;
-
-import com.rest_db.crudrestdb.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
--- save_db/src/main/java/com/hibernet/intro1/dao/StudentDAO.java
+++
@@ -1,7 +0,0 @@
-package com.hibernet.intro1.dao;
-
-import com.hibernet.intro1.entity.Student;
-
-public interface StudentDAO {
-    void save(Student thestudent);
-}

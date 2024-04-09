c93f9f8100e1aa64f3185fef7dcb3971de4a7789
akhushal <khushal0042@gmail.com>
2024-04-09 22:28:23 +0530

    spring boot with hibernet and rest services

---
+++ read_search_query_update/src/main/java/com/hibernet/intro1/dao/StudentDAOimpl.java
@@ -0,0 +1,56 @@
+package com.hibernet.intro1.dao;
+
+import com.hibernet.intro1.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class StudentDAOimpl implements StudentDAO {
+
+    //field for entity manager
+    private EntityManager entityManager;
+    private Student theStudent;
+
+    //inject entity manager using constructor injection
+    @Autowired
+    public StudentDAOimpl(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+    //implement save method
+    @Override
+    @Transactional
+    public void save(Student theStudent){
+        entityManager.persist(theStudent);
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+        TypedQuery<Student> thelastName = entityManager.createQuery("FROM Student WHERE lastName=:theData", Student.class);
+        thelastName.setParameter("theData", theLastName);
+        return thelastName.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+    }
+
+
+}
---
+++ save_db/src/main/java/com/hibernet/intro1/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.hibernet.intro1.dao;
+
+import com.hibernet.intro1.entity.Student;
+
+public interface StudentDAO {
+    void save(Student thestudent);
+}
